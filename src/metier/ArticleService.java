e7041776d32f7bbed1d3b2872fec435ec80d3d01
Elina MILLET <millet.elina@gmail.com>
2016-07-07 14:29:14 +0200

    ArticleService + jar

---
+++ src/metier/ArticleService.java
@@ -0,0 +1,24 @@
+package metier;
+
+import java.util.ArrayList;
+
+import domaine.Article;
+
+public class ArticleService {
+	private ArrayList<Article> catalogue = new ArrayList<>();
+
+	public ArticleService() {
+		Article article1 = new Article(1, "bordeaux1", 100);
+		Article article2 = new Article(2, "bordeaux2", 200);
+		Article article3 = new Article(3, "bordeaux3", 300);
+
+		catalogue.add(article1);
+		catalogue.add(article2);
+		catalogue.add(article3);
+	}
+
+	ArrayList<Article> getCatalogue() {
+		return catalogue;
+
+	}
+}
